d6f895083ddd4f0ec5167455cacab616888bd89d
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-10-11 17:45:44 +0300

    initial commit

---
+++ SwiftMacrosExplanation/SwiftMacrosExplanation/SwiftMacrosExplanationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMacrosExplanationApp.swift
+//  SwiftMacrosExplanation
+//
+//  Created by Ahmed Amin on 11/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMacrosExplanationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
